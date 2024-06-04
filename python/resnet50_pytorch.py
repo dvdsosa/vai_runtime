c11db4233ec682b8cac3c42eb4fb465db2411894
David Sosa Trejo <dvdsosa@gmail.com>
2024-06-04 15:04:59 -0700

    feat: load images in batchs using pytorch
    what: use pytorch functions to load images using
    folders and subfolders
    why: to be able to process at least 9000 images at
    once
    how: new code added

--- python/resnet50_pytorch.py
+++ python/resnet50_pytorch.py
@@ -16,6 +16,7 @@ limitations under the License.
 
 from ctypes import *
 from typing import List
+from tqdm import tqdm
 import cv2
 import numpy as np
 import xir
@@ -26,10 +27,15 @@ import threading
 import time
 import sys
 import re
+import random
+import torch
+from torchvision import transforms, datasets
 
 # Declarar las variables globales
 inferred_classes = []
 ground_truth_classes = []
+start_time=[]
+start_time_2=[]
 
 """
 Calculate softmax
@@ -56,70 +62,31 @@ Get topk results according to its probability
 datain: data result of softmax
 filePath: filePath in witch that records the infotmation of kinds
 """
-def TopK(datain, size, ground_truth_filename, data1):
+def TopK(softmax, read_image_filename, target_classes):
 
     global inferred_classes
     global ground_truth_classes
 
-    cnt = [i for i in range(size)]
-    pair = zip(datain, cnt)
-    pair = sorted(pair, reverse=True)
-    softmax_new, cnt_new = zip(*pair)
+    max_value=max(softmax)
+    max_index=softmax.index(max_value)
 
-    inferred_class_id = int(data1[cnt_new[0]].strip("\n").split('_')[0])
-    ground_truth_class_id = int(re.match(r'(\d+)', ground_truth_filename).group(1))
+    inferred_class_id = int(target_classes[max_index].strip("\n").split('_')[0])
+    ground_truth_class_id = int(re.search(r'/(\d+)_', read_image_filename[0]).group(1))
+
     # index -->  cnt_new[0]+1
     #print("Top[%d] Inferenced --> %02d, %02d <-- Ground Truth. Queried filename %s" % (0, inferred_class_id, ground_truth_class_id, ground_truth_filename))
 
     inferred_classes.append(inferred_class_id)
     ground_truth_classes.append(ground_truth_class_id)
 
-"""
-pre-process for resnet50 (caffe)
-"""
-_B_MEAN = 10.455
-_G_MEAN = 9.0525
-_R_MEAN = 10.6845
-MEANS = [_B_MEAN, _G_MEAN, _R_MEAN]
-SCALES = [0.05086340632, 0.0429525589, 0.04089226932]
-
-def preprocess_one_image_fn(image_path, fix_scale, width=224, height=224):
-    means = MEANS
-    scales = SCALES
-    image = cv2.imread(image_path)
-    # Resize the image to 256 on the smallest side
-    smallest_side = 256.0
-    scale = smallest_side / min(image.shape[0], image.shape[1])
-    new_size = (int(image.shape[1]*scale), int(image.shape[0]*scale))
-    resized_image = cv2.resize(image, new_size)
-    
-    # Center crop the image
-    y, x, _ = resized_image.shape
-    startx = x//2 - (width//2)
-    starty = y//2 - (height//2)
-    crop_img = resized_image[starty:starty+height, startx:startx+width]
-
-    B, G, R = cv2.split(crop_img) 
-    B = (B - means[0]) * scales[0] * fix_scale
-    G = (G - means[1]) * scales[1] * fix_scale
-    R = (R - means[2]) * scales[2] * fix_scale
-    image = cv2.merge([B, G, R])
-    image = image.astype(np.int8)
-    return image
-
-
-SCRIPT_DIR = get_script_directory()
-calib_image_dir = SCRIPT_DIR + "/../../DYB_val_full"
 global threadnum
 threadnum = 0
 """
 run resnt50 with batch
 runner: dpu runner
-img: imagelist to be run
-cnt: threadnum
+val_loader: dataset to be run
 """
-def runResnet50(runner: "Runner", img, ground_truth_filename, cnt):
-
+def runResnet50(runner: "Runner", val_loader):
     """get tensor"""
     inputTensors = runner.get_input_tensors()
     outputTensors = runner.get_output_tensors()
@@ -129,33 +96,32 @@ def runResnet50(runner: "Runner", img, ground_truth_filename, cnt):
     output_ndim = tuple(outputTensors[0].dims)
     output_fixpos = outputTensors[0].get_attr("fix_point")
     output_scale = 1 / (2**output_fixpos)
-    n_of_images = len(img)
 
     # Read the file once outside the function
     with open("./plankton_list.txt", "r") as fp:
-        data1 = fp.readlines()
+        target_classes = fp.readlines()
 
-    count = 0
-    while count < cnt:
-        runSize = input_ndim[0]
-        """prepare batch input/output """
-        inputData = [np.empty(input_ndim, dtype=np.int8, order="C")]
-        outputData = [np.empty(output_ndim, dtype=np.int8, order="C")]
-        """init input image to input buffer """
-        for j in range(runSize):
-            imageRun = inputData[0]
-            imageRun[j, ...] = img[(count + j) % n_of_images].reshape(input_ndim[1:])
-        """run with batch """
-        job_id = runner.execute_async(inputData, outputData)
-        runner.wait(job_id)
-        """softmax&TopK calculate with batch """
-        """Benchmark DPU FPS performance over Vitis AI APIs execute_async() and wait() """
-        """Uncomment the following code snippet to include softmax calculation for model’s end-to-end FPS evaluation """
-        for j in range(runSize):
-            softmax = CPUCalcSoftmax(outputData[0][j], pre_output_size, output_scale)
-            TopK(softmax, pre_output_size, ground_truth_filename[count], data1)
-
-        count = count + runSize
+    try: 
+        for image, _, read_image_filename in val_loader:
+            runSize = input_ndim[0]
+            """prepare batch input/output """
+            inputData = [np.empty(input_ndim, dtype=np.int8, order="C")]
+            outputData = [np.empty(output_ndim, dtype=np.int8, order="C")]
+            """init input image to input buffer """
+            for j in range(val_loader.batch_size):
+                imageRun = inputData[0]
+                imageRun[j, ...] = image[j].permute(1, 2, 0)
+            """run with batch """
+            job_id = runner.execute_async(inputData, outputData)
+            runner.wait(job_id)
+            """softmax&TopK calculate with batch """
+            """Benchmark DPU FPS performance over Vitis AI APIs execute_async() and wait() """
+            """Uncomment the following code snippet to include softmax calculation for model’s end-to-end FPS evaluation """
+            for j in range(runSize):
+                softmax = CPUCalcSoftmax(outputData[0][j], pre_output_size, output_scale)
+                TopK(softmax, read_image_filename, target_classes)
+    except KeyboardInterrupt:
+        print("\nInterruption detected, stopping the loop...")
 
 """
  obtain dpu subgrah
@@ -174,32 +140,64 @@ def get_child_subgraph_dpu(graph: "Graph") -> List["Subgraph"]:
         if cs.has_attr("device") and cs.get_attr("device").upper() == "DPU"
     ]
 
+class CustomImageFolder(datasets.ImageFolder):
+    # This method is called when an item from the dataset is accessed
+    def __getitem__(self, index):
+        # Get the image and label as you normally would
+        image, label = super().__getitem__(index)
+        # Get the image file name
+        image_file = self.samples[index][0]
+        # Return the image, label, and file name
+        return image, label, image_file
+
+def load_data(data_dir='dataset/imagenet',
+              batch_size=1,
+              subset_len=None,
+              sample_method='random',
+              fix_scale=8,
+              **kwargs):
+
+    means = [10.6845, 9.0525, 10.455]
+    scales = [0.04089226932, 0.0429525589, 0.05086340632]
+    #normalize = transforms.Normalize(mean=means, std=scales)
+    val_transform = transforms.Compose([
+        transforms.Resize(256),  # Resize the smallest side to 256
+        transforms.CenterCrop(224),  # Crop the center of the image
+        transforms.ToTensor(),  # Convert the image to a tensor
+        transforms.Lambda(lambda x: x[[2, 1, 0], :, :]),  # Change the channel order from RGB to BGR
+        transforms.Lambda(lambda x: x * 255),
+        transforms.Lambda(lambda x: (x - torch.tensor(means).view(3, 1, 1)) * torch.tensor(scales).view(3, 1, 1) * fix_scale),  # Apply the normalization
+        transforms.Lambda(lambda x: x.to(torch.int8)),  # Convert the tensor to integers
+    ])
+    dataset = CustomImageFolder(root=data_dir, transform=val_transform)
+
+    if subset_len:
+        assert subset_len <= len(dataset)
+        if sample_method == 'random':
+            dataset = torch.utils.data.Subset(dataset, random.sample(range(0, len(dataset)), subset_len))
+        else:
+            dataset = torch.utils.data.Subset(dataset, list(range(subset_len)))
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, **kwargs)
+
 def main(argv):
     global threadnum
     global start_time
 
-    # Verificar si OpenCV está compilado con soporte para OpenCL
-    #if cv2.ocl.haveOpenCL():
-    #    print("OpenCV fue compilado con soporte para OpenCL.")
-    #else:
-    #    print("OpenCV no fue compilado con soporte para OpenCL.")
-
+    threadAll = []
      # Fixed to 1 due the maximum DPU cores that can be implemented in the Kria KV260 is one
     threadnum = 1
 
-    listimage = os.listdir(calib_image_dir)
-    threadAll = []
-    runTotal = len(listimage)
-
     # Check if the user has introduced the quantity of images to process
     if len(argv) > 2:
-        if int(argv[2]) > 0:
-            image_count = int(argv[2])
+        image_count = int(argv[2])
+        if image_count > 0:
+            subset_len = image_count
         else:
-            image_count = runTotal
+            subset_len = None
     else:
-        image_count = runTotal
+        subset_len = None
 
+    # Load the .xmodel to perform the inference
     g = xir.Graph.deserialize(argv[1])
     subgraphs = get_child_subgraph_dpu(g)
     assert len(subgraphs) == 1  # only one DPU kernel
@@ -210,35 +208,33 @@ def main(argv):
     input_fixpos = all_dpu_runners[0].get_input_tensors()[0].get_attr("fix_point")
     input_scale = 2**input_fixpos
 
-    # Marca el tiempo de inicio
+    # Set the start time
     start_time = time.time()
-    """image list to be run """
-    img = []
-    # List of integers
-    ground_truth_filename = []
-    for i in range(image_count):
-        path = os.path.join(calib_image_dir, listimage[i])
-        img.append(preprocess_one_image_fn(path, input_scale))
-        ground_truth_filename.append(listimage[i])
 
+#    data_dir= '../../DYB-original/test',
+#    data_dir= '../../test02/',
+    val_loader = load_data(
+        data_dir= '../../DYB-original/test',
+        batch_size=1,
+        subset_len=subset_len,
+        sample_method='random',
+        fix_scale=input_scale
+        )
+
+    # When the user has not introduced the quantity of images to process
+    if 'image_count' not in locals():
+        image_count = len(val_loader)
+    
     end_time_1 = time.time()
     diff_1 = (end_time_1 - start_time) * 1000
 
-    """
-      The cnt variable is used to control the number of times a single-thread DPU runs.
-      Users can modify the value according to actual needs. It is not recommended to use
-      too small number when there are few input images, for example:
-      1. If users can only provide very few images, e.g. only 1 image, they should set
-         a relatively large number such as 360 to measure the average performance;
-      2. If users provide a huge dataset, e.g. 50000 images in the directory, they can
-         use the variable to control the test time, and no need to run the whole dataset.
-    """
-    cnt = image_count
+    # for iteration, (images, labels) in enumerate(val_loader):
+    #     print(labels)
 
     start_time_2 = time.time()
     """run with batch """
     for i in range(int(threadnum)):
-        t1 = threading.Thread(target=runResnet50, args=(all_dpu_runners[i], img, ground_truth_filename, cnt))
+        t1 = threading.Thread(target=runResnet50, args=(all_dpu_runners[i], val_loader))
         threadAll.append(t1)
     for x in threadAll:
         x.start()
@@ -252,13 +248,14 @@ def main(argv):
     print(f"Elapsed time for dataset image loading: {diff_1} ms")
     print(f"Elapse time since for inference: {timetotal} ms")
 
-    total_frames = cnt * int(threadnum)
+    total_frames = image_count * int(threadnum)
     fps = float(total_frames / timetotal)
     print(
         "FPS=%.2f, total frames = %.2f , time=%.6f seconds"
         % (fps, total_frames, timetotal)
     )
-    # Calcular la precisión
+
+    # Calculate accuracies
     true_positives = 0
     false_positives = 0
     false_negatives = 0
@@ -276,11 +273,11 @@ def main(argv):
     recall = true_positives / (true_positives + false_negatives)
     f1_score = 2 * (precision * recall) / (precision + recall)
 
-    # Imprimir los resultados
+    # Print results
     print("Accuracy: %.2f%%, Precision: %.2f%%, Recall: %.2f%%, F1 Score: %.2f%%" % (accuracy*100, precision*100, recall*100, f1_score*100))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("usage : python3 resnet50.py <resnet50_xmodel_file> [thread_number]")
+        print("usage : python3 resnet50_pytorch.py <resnet50_xmodel_file> [thread_number]")
     else:
         main(sys.argv)
--- readme.md
+++ readme.md
@@ -39,9 +39,11 @@ Source code forked from [Vitis-AI repo](https://github.com/Xilinx/Vitis-AI/tree/
 
 `cd ~/vai_runtime/python`
 
-`sudo python resnet50.py 1 ../resnet50_DYB.xmodel`
+`sudo python resnet50.py ../resnet50_DYB.xmodel [number_of_images_to_process]`
 
-# Steps to change the permissions of  the DPU device
+`sudo python resnet50_pytorch.py ../resnet50_DYB.xmodel [number_of_images_to_process]`
+
+# Steps to change the permissions of the DPU device
 When running any of the above Python or C++ programs, these need to be executed as root by using the sudo command. This is because the dpu device needs root permissions, as shown below:
 
 `ls -l /dev/dpu`
@@ -61,3 +63,64 @@ Now, you can see the changes by:
 If you wish to restore the initial permissions of the /dev/dpu device, then:
 
 `sudo chmod u=rw,g=,o= /dev/dpu`
+
+# RESULTS
+
+
+## C++
+
+Para 1000 y 2000 imágenes respectivamente.
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime$ ./resnet50_pt resnet50_DYB.xmodel ../targetKria 1000
+Average FPS: 66.5283
+Accuracy: 92.9%, Precision: 99.8925%, Recall: 92.993%, F1 Score: 96.3193%
+xilinx-kv260-starterkit-20232:~/vai_runtime$ ./resnet50_pt resnet50_DYB.xmodel ../targetKria 2000
+Average FPS: 66.578
+Accuracy: 93.5%, Precision: 100%, Recall: 93.5%, F1 Score: 96.6408%
+```
+
+Tiempo total desde que lanzo el script, carga las imágenes, y las procesa:
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime$ ./run_resnet50_cpp.sh 
+Average FPS: 38.7914
+Accuracy: 92.9%, Precision: 99.8925%, Recall: 92.993%, F1 Score: 96.3193%
+El comando se ejecutó en 26 segundos.
+FPS: 38.46
+```
+
+## Python
+
+Para 1000 imágenes, con precisión decimal casi idéntica a C++.
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime/python$ python resnet50.py 1 ../resnet50_DYB.xmodel
+FPS=81.72, total frames = 1000.00 , time=12.237606 seconds
+Accuracy: 73.90%, Precision: 99.60%, Recall: 74.12%, F1 Score: 84.99%
+```
+Para 2000 imágenes, con precisión decimal casi idéntica a C++.
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime/python$ python resnet50.py 1 ../resnet50_DYB.xmodel
+FPS=81.30, total frames = 2000.00 , time=24.598747 seconds
+Accuracy: 74.00%, Precision: 100.00%, Recall: 74.00%, F1 Score: 85.06%
+```
+
+Para 1000 imágenes, con una normalización de los canales según script original de Vitis-AI.
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime/python$ python resnet50.py 1 ../resnet50_DYB.xmodel
+FPS=80.66, total frames = 1000.00 , time=12.398101 seconds
+Accuracy: 83.20%, Precision: 99.88%, Recall: 83.28%, F1 Score: 90.83%
+```
+Para 2000 imágenes, con una normalización de los canales según script original de Vitis-AI.
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime/python$ python resnet50.py 1 ../resnet50_DYB.xmodel
+FPS=80.36, total frames = 2000.00 , time=24.886486 seconds
+Accuracy: 83.55%, Precision: 100.00%, Recall: 83.55%, F1 Score: 91.04%
+```
+
+Tiempo total desde que lanzo el script, carga las imágenes, y las procesa:
+```json
+xilinx-kv260-starterkit-20232:~/vai_runtime/python$ ./run_resnet50_python.sh 
+FPS=32.19, total frames = 1000.00 , time=31.062088 seconds
+Accuracy: 83.20%, Precision: 99.88%, Recall: 83.28%, F1 Score: 90.83%
+El comando se ejecutó en 33 segundos.
+FPS: 30.30
+```
